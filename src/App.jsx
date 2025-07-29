1fa8b80e009f0e6a823c4a465f9951c4f3f34a4b
Alekhya <2300032926cseh1@gmail.com>
2025-07-29 12:34:18 +0530

    updated App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './style.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Welcome to My React Project - S205</h1>
+      <p>CI/CD and cloud devops</p>
+      <p>This is a simple homepage deployed on GitHub Actions using Vite.</p>
+    </div>
+  );
+}
+
+export default App;
